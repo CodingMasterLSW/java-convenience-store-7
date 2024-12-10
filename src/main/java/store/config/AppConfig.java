c8f0428563af9ef4129397df4ffe2c6892cadd4f
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-12-10 13:28:12 +0900

    feat: MVC 패턴 기반 적용

---
+++ src/main/java/store/config/AppConfig.java
@@ -0,0 +1,21 @@
+package store.config;
+
+import store.controller.StoreController;
+import store.service.StoreService;
+import store.view.InputView;
+import store.view.OutputView;
+
+public class AppConfig {
+
+    private AppConfig() {
+    }
+
+    public static StoreController createController() {
+        return new StoreController(InputView.create(), OutputView.create(), createService());
+    }
+
+    public static StoreService createService() {
+        return new StoreService();
+    }
+
+}
---
+++ src/main/java/store/view/InputView.java
@@ -0,0 +1,32 @@
+package store.view;
+
+import static store.view.ErrorMessage.NOT_BLANK_INPUT;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private InputView() {
+    }
+
+    public static InputView create() {
+        return new InputView();
+    }
+
+    public String userInput() {
+        String userInput = Console.readLine();
+        validateInput(userInput);
+        return userInput;
+    }
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    private void validateInput(String userInput) {
+        if (userInput.isBlank() || userInput == null) {
+            throw new IllegalArgumentException(NOT_BLANK_INPUT.getMessage());
+        }
+    }
+
+}
